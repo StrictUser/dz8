f3f36b53ef134e09ad6b1d2f83912b0617955c8f
StrictUser <ermolenko198528@gmail.com>
2018-08-05 12:47:26 +0200

    Completed hometask #8

---
+++ while.py
@@ -0,0 +1,14 @@
+total = 100
+ 
+while total > 0:
+    try:
+        n = int(input("Enter some number: "))
+        if (total - n < 0):
+            print("Be careful - you can't use more than you have! Try again.")
+            n = int(input("Enter some number: "))
+        total = total - n
+        print("Now you resorce is", total)
+    except ValueError:
+        print("You entered not a number! Try again.")
+ 
+print("Resource exhausted!")
