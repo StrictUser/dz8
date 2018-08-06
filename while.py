e48613f7fe124913d3209c486065027f4d28ad90
StrictUser <ermolenko198528@gmail.com>
2018-08-06 07:12:58 +0200

    Completed 2d task in Homework #8

--- while.py
+++ while.py
@@ -1,14 +1,25 @@
-total = 100
+## task 1
+
+# total = 100
  
-while total > 0:
-    try:
-        n = int(input("Enter some number: "))
-        if (total - n < 0):
-            print("Be careful - you can't use more than you have! Try again.")
-            n = int(input("Enter some number: "))
-        total = total - n
-        print("Now you resorce is", total)
-    except ValueError:
-        print("You entered not a number! Try again.")
+# while total > 0:
+#     try:
+#         n = int(input("Enter some number: "))
+#         if (total - n < 0):
+#             print("Be careful - you can't use more than you have! Try again.")
+#             n = int(input("Enter some number: "))
+#         total = total - n
+#         print("Now you resorce is", total)
+#     except ValueError:
+#         print("You entered not a number! Try again.")
  
-print("Resource exhausted!")
+# print("Resource exhausted!")
+
+
+## task 2
+
+i = 0
+
+while i <= 20:
+    print("2**%d =" % i, 2**i)
+    i+=1
